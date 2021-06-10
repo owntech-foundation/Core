dc309e52b031c014d9a8ef44ffd02dc50e2e97f3
Clément Foucher <cfoucher@laas.fr>
2021-06-10 09:39:31 +0000

    Use timer 7 for OwnTech data acquisition module so that it can run fully in background without requiring user calls.

--- zephyr/modules/owntech_data_acquisition/zephyr/data_acquisition.c
+++ zephyr/modules/owntech_data_acquisition/zephyr/data_acquisition.c
@@ -22,21 +22,52 @@
  */
 
 
+/////
+// OwnTech Power API includes
+
+#include "timer.h"
 #include "dma/dma.h"
 #include "adc/adc.h"
+#include "data_dispatch/data_dispatch.h"
 
 
 /////
-// Public functions
+// Timer
+
+#define TIMER7_LABEL DT_PROP(DT_NODELABEL(timers7), label)
+static const struct device* timer7;
+
+
+/////
+// Private functions
+
+
+/////
+// Public API
 
 void data_acquisition_init()
 {
-    /////
-    // Initialize peripherals
+	/////
+	// Initialize peripherals
 
-    // ADC
-    adc_init();
+	// ADC
+	adc_init();
 
-    // DMA
-    dma_init();
+	// DMA
+	dma_init();
+
+	/////
+	// Initialize data dispatch
+	data_dispatch_init();
+
+	/////
+	// Configure timer for background data dispatch
+	timer7 = device_get_binding(TIMER7_LABEL);
+	struct timer_config_t timer_cfg =
+	{
+		.timer_enable_irq = 1,
+		.timer_callback   = data_dispatch_do_dispatch
+	};
+	timer_config(timer7, &timer_cfg);
+	timer_start(timer7, 25);
 }
