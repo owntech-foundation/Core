a6d5fcf109d109e2692ed76082d47c03403af5c0
Clément Foucher <cfoucher@laas.fr>
2022-07-12 11:57:09 +0000

    Update to latest platform.

--- zephyr/platformio-extra.py
+++
@@ -1,8 +0,0 @@
-
-# Custom settings, as referred to as "extra_script" in platformio.ini
-#
-# See https://docs.platformio.org/en/latest/projectconf/section_env_advanced.html#extra-scriptss
-
-Import("env")
-
-env.Append( CXXFLAGS=[ "-Wno-register -fno-rtti" ] )
