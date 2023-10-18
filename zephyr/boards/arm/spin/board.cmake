49ee8ea9055f9d934e96a84a2265d36e198ae552
Clément Foucher <cfoucher@laas.fr>
2023-10-18 09:05:18 +0200

    Clean redundant Kconfig. Update board CMake.

--- zephyr/boards/arm/spin/board.cmake
+++ zephyr/boards/arm/spin/board.cmake
@@ -4,9 +4,9 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-# use target=stm32g474rbtx instead of stm32g474retx
-# to allow board re-flashing (see PR #23230)
-board_runner_args(pyocd "--target=stm32g474rbtx")
+board_runner_args(pyocd "--target=stm32g474retx")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
