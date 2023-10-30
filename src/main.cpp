d3cca1c7256592cf6179b08f0ce6a2b8a09b1218
Luiz Villa <lflavado@laas.fr>
2023-10-30 12:19:12 +0100

    Updated the main.cpp to be an empty canvas

--- src/main.cpp
+++ src/main.cpp
@@ -27,11 +27,87 @@
  *
  * @author  Clément Foucher <clement.foucher@laas.fr>
  */
+//-------------OWNTECH DRIVERS-------------------
+#include "HardwareConfiguration.h"
+#include "DataAcquisition.h"
+#include "Scheduling.h"
+#include "power.h"
 
+//------------ZEPHYR DRIVERS----------------------
+
+//--------------SETUP FUNCTIONS DECLARATION-------------------
+void setup_hardware(); //setups the hardware peripherals of the system
+void setup_software(); //setups the scheduling of the software and the control method
+
+//-------------LOOP FUNCTIONS DECLARATION----------------------
+void loop_communication_task(); //code to be executed in the slow communication task
+void loop_application_task();   //code to be executed in the fast application task
+void loop_control_task();       //code to be executed in real-time at 20kHz
+
+int8_t communication_task_number; //holds the number of the communication task
+int8_t application_task_number; //holds the number of the application task
+
+
+enum serial_interface_menu_mode //LIST OF POSSIBLE MODES FOR THE OWNTECH CONVERTER
+{
+    IDLEMODE =0,
+};
+
+uint8_t received_serial_char;
+uint8_t mode = IDLEMODE;
+
+//--------------USER VARIABLES DECLARATIONS----------------------
+
+
+
+//---------------------------------------------------------------
+
+
+//---------------SETUP FUNCTIONS----------------------------------
+
+void setup_hardware()
+{
+    hwConfig.setBoardVersion(TWIST_v_1_1_2);
+    //setup your hardware here
+}
+
+void setup_software()
+{
+    application_task_number = scheduling.defineAsynchronousTask(loop_application_task);
+    scheduling.startAsynchronousTask(application_task_number);
+    //setup your software scheduling here
+}
+
+//---------------LOOP FUNCTIONS----------------------------------
+
+void loop_communication_task()
+{
+        //communication task code goes here
+}
+
+
+void loop_application_task()
+{
+        printk("Hello World! \n");
+        hwConfig.setLedToggle();
+
+        scheduling.suspendCurrentTaskMs(100);
+}
+
+
+void loop_control_task()
+{
+    //loop control task code goes here
+}
+
+/**
+ * This is the main function of this example
+ * This function is generic and does not need editing.
+ */
 
 int main(void)
 {
-
-
+    setup_hardware();
+    setup_software();
 	return 0;
 }
