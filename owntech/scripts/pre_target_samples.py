e8d8c3d77fa0d95192dc6343a051c58ef7c5e084
Jean Alinei <jean.alinei@laas.fr>
2024-02-12 14:33:25 +0100

    Major script improvement : Example pulling is now done pre-build. There is no more requirements of having a main successfully compiling in order to build an example.
    Signed-off-by: Jean Alinei <jean.alinei@laas.fr>

--- owntech/scripts/pre_target_samples.py
+++ owntech/scripts/pre_target_samples.py
@@ -1,14 +1,18 @@
 import helper_functions
 import os
+import click
 import shutil
 import subprocess
 import functools
+
+from SCons.Script import COMMAND_LINE_TARGETS  # pylint: disable=import-error
 
 Import("env")
 
 sample_dir = os.path.join(".", "owntech", "samples")
 src_dir = os.path.join(".", "src")
 old_dir = os.path.join(".", "old")
+build_dir = os.path.join(".", ".pio", "build")
 
 targets = [
     {
@@ -20,24 +24,38 @@ targets = [
         "sample_url": "https://github.com/foobar/RC1.zip",
     },
     {
-        "name": "Buck_Voltage_Mode-RC1",
+        "name": "example_buck_voltage_mode-RC1",
         "title": "Voltage Mode Buck",
         "description": "Voltage mode buck converter using PID controller for TWIST",
         "group": "Samples TWIST",
         "file": "RC1.zip",
         "sample_url": "https://github.com/owntech-foundation/Buck_Voltage_Mode/archive/refs/tags/RC1.zip",
     },
+    {
+        "name": "example_twist_boost_voltage_mode-RC1",
+        "title": "Voltage Mode Boost",
+        "description": "Voltage mode boost converter using PID controller for TWIST",
+        "group": "Samples TWIST",
+        "file": "RC1.zip",
+        "sample_url": "https://github.com/owntech-foundation/example_twist_boost_voltage_mode/archive/refs/tags/RC1.zip",
+    },
+    {
+        "name": "example_twist_interleaved-RC1",
+        "title": "Interleaved Mode Buck",
+        "description": "Interleaved mode buck converter using PID controller for TWIST",
+        "group": "Samples TWIST",
+        "file": "RC1.zip",
+        "sample_url": "https://github.com/owntech-foundation/example_twist_interleaved/archive/refs/tags/RC1.zip",
+    },
     # Add more targets as needed
 ]
 
-def pullSampleGeneric(extra_arg, target, source, env):
+def pullSampleGeneric(extra_arg):
     target_info = extra_arg    
     if target_info is None:
         print("No target_info found for target")
-    
     file = target_info["file"]
     sample_url = target_info["sample_url"]
-
     res = helper_functions.check_file_and_download(target_info["name"], sample_dir, file, sample_url)
     if res != 0:
         exit(-1)
@@ -69,14 +87,28 @@ def pullSampleGeneric(extra_arg, target, source, env):
 def create_pull_sample_function(arg_value):
     return functools.partial(pullSampleGeneric, extra_arg=arg_value)
 
+def get_pull_sample_function(target_name):
+    for target_info in targets:
+        if target_info["name"] == target_name:
+            return create_pull_sample_function(target_info)
+
+def PrintSuccess(target, source, env):
+    return
+
+# Add the build middleware callback
+
 for target_info in targets:
-    pullSampleSpecific=create_pull_sample_function(target_info)
     env.AddTarget(
         name=target_info["name"],
         dependencies=None,
-        actions=env.VerboseAction(pullSampleSpecific, "Retrieving desired sample.."),
+        actions=env.VerboseAction(PrintSuccess,"Example succesfully loaded and built, enjoy !"),
         title=target_info["title"],
         description=target_info["description"],
         group=target_info["group"],
-        always_build=False,
+        always_build=True,
     )
+
+for targets_info in targets: 
+    if targets_info["name"] in COMMAND_LINE_TARGETS:
+        pullSampleGeneric(target_info)
+        shutil.rmtree(build_dir)
