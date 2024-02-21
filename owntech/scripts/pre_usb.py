35c8303a7474368c21410cbaf34c08b85b6cef75
Jean Alinei <jean.alinei@laas.fr>
2024-02-21 14:44:36 +0100

    [Bug-Fix] #3 monitor for bootloader_stlink environment now working by changing zephyr,console choosen property with a pre script.
    Signed-off-by: Jean Alinei <jean.alinei@laas.fr>

---
+++ owntech/scripts/pre_usb.py
@@ -0,0 +1,28 @@
+import os
+
+def stlink_to_usb():
+    # Specify the dts path
+    dts_path = os.path.join(".", "zephyr", "boards", "arm", "spin", "spin.dts")
+
+    # Define the search and replace strings
+    search_str = "zephyr,console = &lpuart1;"
+    replace_str = "zephyr,console = &cdc_acm_uart0;"
+
+    # Read the contents of the file
+    with open(dts_path, 'r') as file:
+        file_contents = file.read()
+
+    # Check if the search string is present in the file
+    if search_str in file_contents:
+        # Replace the search string with the replace string
+        new_contents = file_contents.replace(search_str, replace_str)
+
+        # Write the updated contents back to the file
+        with open(dts_path, 'w') as file:
+            file.write(new_contents)
+
+        print(f"Replacement of '{search_str}' with '{replace_str}' in '{dts_path}' completed.")
+    else:
+        print(f"'{search_str}' not found in '{dts_path}'. No replacement performed.")
+
+stlink_to_usb()
