22e091e040fb4ba31fe4c4eb6746d402e42a0e15
Jean Alinei <jean.alinei@laas.fr>
2024-05-15 15:38:30 +0200

    Fixing typo in spdx identifier.
    Signed-off-by: Jean Alinei <jean.alinei@laas.fr>

--- zephyr/modules/owntech_spin_api/zephyr/src/TimerHAL.cpp
+++ zephyr/modules/owntech_spin_api/zephyr/src/TimerHAL.cpp
@@ -14,7 +14,7 @@
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: LGLPV2.1
+ * SPDX-License-Identifier: LGPL-2.1
  */
 
 /**
