7c851355d6a49b5cc2c8e284f9acb58e1e64b1e2
Luiz Villa <luiz.villa@laas.fr>
2024-08-29 13:40:26 +0200

    Updates the power class to elimiate the funciton calls that contain All and Leg in them.
    - Introduces ALL_LEGS to allow the users to use only one call for all the legs if necessary.
    - Updates the safety_setting that depends on the power class.

--- zephyr/modules/owntech_shield_api/zephyr/src/Power.h
+++ zephyr/modules/owntech_shield_api/zephyr/src/Power.h
@@ -40,6 +40,7 @@
 typedef enum
 {
 	DT_FOREACH_CHILD_STATUS_OKAY(DT_NODELABEL(powershield), LEG_TOKEN)
+	ALL
 } leg_t;
 
 class PowerAPI
@@ -60,21 +61,8 @@ public:
 	 * 						 The other will be complementary - PWMx1 or PWMx2
 	 * @param leg_mode PWM mode - VOLTAGE_MODE or CURRENT_MODE
 	 */
-	void initLegMode(leg_t leg, 											\
+	void initMode(leg_t leg, 											\
 					 hrtim_switch_convention_t leg_convention,				\
-					 hrtim_pwm_mode_t leg_mode);
-
-	/**
-	 * @brief Initialize power modes for all legs.
-	 *
-	 * This function initializes the power modes  for each leg with the
-	 * specified 'buck' and 'voltage_mode' settings.
-	 *
-	 * @param leg_convention PWM Switch to be driven by the duty cycle. The
-	 * 						 other will be complementary - PWMx1 or PWMx2
-	 * @param leg_mode PWM mode - VOLTAGE_MODE or CURRENT_MODE
-	 */
-	void initAllMode(hrtim_switch_convention_t leg_convention, 				\
 					 hrtim_pwm_mode_t leg_mode);
 
 	/**
@@ -88,19 +76,8 @@ public:
 	 * @param duty_leg The duty cycle value to set
 	 * 				   (a floating-point number between 0.1 and 0.9).
 	 */
-	void setLegDutyCycle(leg_t leg, float32_t duty_leg);
+	void setDutyCycle(leg_t leg, float32_t duty_leg);
 
-	/**
-	 * @brief Set the duty cycle for power control of all the legs.
-	 *
-	 * This function sets the same duty cycle for power control of all the legs.
-	 * The duty cycle determines the ON/OFF ratio of the power signal for all
-	 * legs.
-	 *
-	 * @param duty_all The duty cycle value to set
-	 * 				   (a floating-point number between 0.1 and 0.9).
-	 */
-	void setAllDutyCycle(float32_t duty_all);
 
 	/**
 	 * @brief Start power output for a specific leg.
@@ -113,48 +90,28 @@ public:
 	 *
 	 * @param leg The leg for which to start the power output.
 	 */
-	void startLeg(leg_t leg);
+	void start(leg_t leg);
 
 	/**
 	 * @brief Connect the electrolytic capacitor.
 	 *
 	 * @param leg The selected leg for which to connect the capacitor.
 	*/
-	void connectLegCapacitor(leg_t leg);
-
-	/**
-	 * @brief Start power output for all legs.
-	 */
-	void startAll();
-
-	/**
-	 * @brief Connect the electrolyt capacitor to all the legs.
-	*/
-	void connectAllCapacitor();
+	void connectCapacitor(leg_t leg);
 
 	/**
 	 * @brief Stop power output for a specific leg.
 	 *
 	 * @param leg The leg for which to stop the power output.
 	 */
-	void stopLeg(leg_t leg);
+	void stop(leg_t leg);
 
 	/**
 	 * @brief Disonnect the electrolytic capacitor.
 	 *
 	 * @param leg The selected leg for which to disonnect the capacitor.
 	*/
-	void disconnectLegCapacitor(leg_t leg);
-
-	/**
-	 * @brief Stop power output for all legs.
-	 */
-	void stopAll();
-
-	/**
-	 * @brief disconnect the electrolyt capacitor to all the legs.
-	*/
-	void disconnectAllCapacitor();
+	void disconnectCapacitor(leg_t leg);
 
 	/**
 	 * @brief Set the trigger value for a specific leg's ADC trigger.
@@ -166,19 +123,7 @@ public:
 	 * @param leg The leg for which to set the ADC trigger value.
 	 * @param trigger_value The trigger value to set  between 0.05 and 0.95.
 	 */
-	void setLegTriggerValue(leg_t leg, float32_t trigger_value);
-
-	/**
-	 * @brief Set the trigger value for the ADC trigger of all the legs.
-	 *
-	 * This function sets the same trigger value for the ADC trigger of all
-	 * the legs. The trigger value determines the level at which the ADC trigger
-	 * is activated for all legs.
-	 *
-	 * @param trigger_value The trigger value to set for all the legs
-	 * 						between 0.0 and 1.0.
-	 */
-	void setAllTriggerValue(float32_t trigger_value);
+	void setTriggerValue(leg_t leg, float32_t trigger_value);
 
 	/**
 	 * @brief Set the phase shift value for a specific leg's power control.
@@ -186,14 +131,7 @@ public:
 	 * @param leg The leg for which to set the phase shift value.
      * @param[in] phase_shift	phase shift value in degrees between -360 and 360
 	 */
-	void setLegPhaseShift(leg_t leg, int16_t phase_shift);
-
-	/**
-	 * @brief Set the phase shift value for all the legs.
-	 *
-     * @param[in] phase_shift	phase shift value in degrees between -360 and 360
-	 */
-	void setAllPhaseShift(int16_t phase_shift);
+	void setPhaseShift(leg_t leg, int16_t phase_shift);
 
 	/**
 	 * @brief Set the slope compensation in current mode for a leg
@@ -206,21 +144,8 @@ public:
 	 *
 	 * @warning Be sure that set_voltage > reset_voltage
 	 */
-	void setLegSlopeCompensation(leg_t leg, 								\
+	void setSlopeCompensation(leg_t leg, 								\
 								 float32_t set_voltage, 					\
-								 float32_t reset_voltage);
-
-	/**
-	 * @brief Set the slope compensation in current mode for all the leg
-	 *
-	 * @param set_voltage in volt, the DAC peak voltage for slope compensation
-	 * 					  (value between 0 and 2.048)
-	 * @param reset_voltage in volt, the DAC lowest voltage for
-	 * 						slope compensation (value between 0 and 2.048)
-	 *
-	 * @warning Be sure that set_voltage > reset_voltage
-	 */
-	void setAllSlopeCompensation(float32_t set_voltage, 					\
 								 float32_t reset_voltage);
 
 	/**
@@ -230,17 +155,9 @@ public:
 	 * @param ns_rising_dt  - rising dead time value in ns
 	 * @param ns_falling_dt - falling dead time value in ns
 	*/
-	void setLegDeadTime(leg_t leg, 											\
+	void setDeadTime(leg_t leg, 											\
 						uint16_t ns_rising_dt, 								\
 						uint16_t ns_falling_dt);
-
-	/**
-	 * @brief set the dead time value for all legs
-	 *
-	 * @param ns_rising_dt  - rising dead time value in ns
-	 * @param ns_falling_dt - falling dead time value in ns
-	*/
-	void setAllDeadTime(uint16_t ns_rising_dt, uint16_t ns_falling_dt);
 
 	/**
 	 * @brief set ADC decimator for a leg
@@ -254,20 +171,7 @@ public:
 	 * @param adc_decim - adc decimator, a number between 1 and 32
 	 *
 	*/
-	void setLegAdcDecim(leg_t leg, uint16_t adc_decim);
-
-	/**
-	 * @brief set ADC decimator for all legs
-	 *
-	 * this function sets the number of event which will be ignored between
-	 * two events. ie. you divide the number of trigger in a fixed period.
-	 * For example if adc_decim = 1, nothing changes but with adc_decims = 2
-	 * you have twice less adc trigger.
-	 *
-	 * @param adc_decim - adc decimator, a number between 1 and 32
-	 *
-	*/
-	void setAllAdcDecim(uint16_t adc_decim);
+	void setAdcDecim(leg_t leg, uint16_t adc_decim);
 
 	/**
 	 * @brief Initialise a leg for buck topology
@@ -276,14 +180,7 @@ public:
 	 * @param leg_mode PWM mode - VOLTAGE_MODE or CURRENT_MODE
 	 *
 	 */
-	void initLegBuck(leg_t leg, hrtim_pwm_mode_t leg_mode = VOLTAGE_MODE);
-
-	/**
-	 * @brief Initialise all the legs for buck topology
-	 *
-	 * @param leg_mode PWM mode - VOLTAGE_MODE or CURRENT_MODE
-	 */
-	void initAllBuck(hrtim_pwm_mode_t leg_mode = VOLTAGE_MODE);
+	void initBuck(leg_t leg, hrtim_pwm_mode_t leg_mode = VOLTAGE_MODE);
 
 	/**
 	 * @brief Initialise all the legs for boost topology, current mode is not
@@ -291,13 +188,8 @@ public:
 	 *
 	 * @param leg Leg to initialize
 	 */
-	void initLegBoost(leg_t leg);
+	void initBoost(leg_t leg);
 
-	/**
-	 * @brief Initialise all the legs for boost topology, current mode is not
-	 *  supported for boost.
-	 */
-	void initAllBoost();
 };
 
 #endif // POWER_H_
