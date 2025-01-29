947363516dfeabf33f475392c7c208f1b2ea95da
Jean Alinei <jean.alinei@owntech.org>
2025-01-29 17:30:51 +0100

    Comments improvement and style fix main.cpp
    Signed-off-by: Jean Alinei <jean.alinei@owntech.org>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2024 LAAS-CNRS
+ * Copyright (c) 2021-present LAAS-CNRS
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Lesser General Public License as published by
@@ -27,69 +27,83 @@
  * @author Luiz Villa <luiz.villa@laas.fr>
  */
 
-//--------------OWNTECH APIs----------------------------------
+/*--------------OWNTECH APIs---------------------------------- */
 #include "TaskAPI.h"
 #include "ShieldAPI.h"
 #include "SpinAPI.h"
 
-//--------------SETUP FUNCTIONS DECLARATION-------------------
-void setup_routine();           /* Setups the hardware and software of the system */
+/*--------------SETUP FUNCTIONS DECLARATION------------------- */
+/* Setups the hardware and software of the system */
+void setup_routine();
 
-//--------------LOOP FUNCTIONS DECLARATION--------------------
-void loop_background_task();    /* Code to be executed in the background task */
-void loop_critical_task();      /* Code to be executed in real time in the critical task */
+/*--------------LOOP FUNCTIONS DECLARATION-------------------- */
 
-//--------------USER VARIABLES DECLARATIONS-------------------
+/* Code to be executed in the background task */
+void loop_background_task();
+/* Code to be executed in real time in the critical task */
+void loop_critical_task();
+
+/*--------------USER VARIABLES DECLARATIONS------------------- */
 
 
 
-//--------------SETUP FUNCTIONS-------------------------------
+/*--------------SETUP FUNCTIONS------------------------------- */
 
 /**
  * This is the setup routine.
  * It is used to call functions that will initialize your hardware and tasks.
- * In this example, we setup the version of the spin board and a 
- * background task. The critical task is defined but not started.
- * NOTE: It is important to follow the steps and initialize the hardware first 
- * and the tasks second. 
+ *
+ * In this default main, we only spawn two tasks
+ *  - A background task.
+ *  - A critical task is defined but not started.
+ *
+ * NOTE: It is important to follow the steps and initialize the hardware first
+ * and the tasks second.
  */
 void setup_routine()
 {
     /* STEP 1 - SETUP THE HARDWARE */
 
     /* STEP 2 - SETUP THE TASKS */
-    uint32_t background_task_number = task.createBackground(loop_background_task);
-    //task.createCritical(loop_critical_task, 500); /* Uncomment if you use the critical task */
+
+    uint32_t background_task_number =
+                            task.createBackground(loop_background_task);
+
+    /* Uncomment the following line if you use the critical task */
+    /* task.createCritical(loop_critical_task, 500); */
 
     /* STEP 3 - LAUNCH THE TASKS */
     task.startBackground(background_task_number);
-    //task.startCritical(); /* Uncomment if you use the critical task */
+
+    /* Uncomment the following line if you use the critical task */
+    /* task.startCritical(); */
 }
 
-//--------------LOOP FUNCTIONS--------------------------------
+/*--------------LOOP FUNCTIONS-------------------------------- */
 
 /**
  * This is the code loop of the background task
- * It is executed second as defined by it suspend task in its last line.
  * You can use it to execute slow code such as state-machines.
+ * The pause define its pseudo-periodicity.
  */
 void loop_background_task()
 {
     printk("Hello World! \n");
     spin.led.toggle();
 
-    task.suspendBackgroundMs(1000);  /* This pauses the task for 1000 mili seconds */
+    /* This pauses the task for 1000 milli seconds */
+    task.suspendBackgroundMs(1000);
 }
 
 /**
  * This is the code loop of the critical task
- * It is executed every 500 micro-seconds defined in the setup_software function.
- * You can use it to execute an ultra-fast code with the highest priority which 
- * cannot be interruped. It is from it that you will control your power flow.
+ * It is executed every 500 micro-seconds defined in the setup_routine function.
+ * You can use it to execute an ultra-fast code with the highest priority which
+ * cannot be interrupted.
  */
 void loop_critical_task()
 {
-    /* This task is left empty in this example */
+    /* This task is left empty in this default main */
 }
 
 /**
