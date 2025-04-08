27cd9e3258a3cc0aee16033289ffda595779256a
Luiz Villa <luiz.villa@laas.fr>
2025-04-08 16:37:30 +0200

    add comments to the .h of the flash driver

--- zephyr/modules/owntech_flash_driver/zephyr/public_api/nvs_storage.h
+++ zephyr/modules/owntech_flash_driver/zephyr/public_api/nvs_storage.h
@@ -55,17 +55,64 @@ typedef enum
  *  API
  */
 
+ /**
+ * @brief Store a data item in non-volatile storage (NVS).
+ *
+ * Stores a block of data under a given identifier (data_id) in flash memory.
+ * If the data already exists, it is overwritten. Useful for persisting configuration.
+ *
+ * @param data_id     Identifier for the data item.
+ * @param data        Pointer to the data to be stored.
+ * @param data_size   Size of the data in bytes.
+ *
+ * @return 0 on success, negative value on error.
+ */
 int8_t nvs_storage_store_data(uint16_t data_id,
 							  const void* data,
 							  uint8_t data_size);
 
+/**
+ * @brief Retrieve a data item from non-volatile storage (NVS).
+ *
+ * Reads the stored data associated with the given identifier (data_id)
+ * and copies it into the provided buffer.
+ *
+ * @param data_id           Identifier for the stored data.
+ * @param data_buffer       Pointer to the buffer where data will be copied.
+ * @param data_buffer_size  Size of the buffer in bytes.
+ *
+ * @return Number of bytes read on success, negative value on error.
+ */							  
 int8_t nvs_storage_retrieve_data(uint16_t data_id,
 								 void* data_buffer,
 								 uint8_t data_buffer_size);
 
+/**
+ * @brief Clear all data stored in the NVS partition.
+ *
+ * Erases all key-value entries in the configured NVS area. Use with caution.
+ *
+ * @return 0 on success, negative value on error.
+ */								 
 int8_t nvs_storage_clear_all_stored_data();
 
+/**
+ * @brief Get the current in-code version of the NVS layout.
+ *
+ * This version corresponds to the structure of data expected by the firmware.
+ * Used to detect incompatibility between NVS layout and firmware logic.
+ *
+ * @return Current version defined in code.
+ */
 uint16_t nvs_storage_get_current_version();
+
+/**
+ * @brief Get the version stored in the NVS flash memory.
+ *
+ * Used to compare with the in-code version to validate compatibility.
+ *
+ * @return Stored version value from flash.
+ */
 uint16_t nvs_storage_get_version_in_nvs();
 
 
